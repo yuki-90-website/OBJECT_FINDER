1e932e6738cf25568586a95d8397ffe7fe411901
yuki-90-website <82995697+yuki-90-website@users.noreply.github.com>
2021-09-28 17:41:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+status = "";
+input = document.getElementById("text");
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
 function start() {
-    var inputtext = document.getElementById("text").value;
-    document.getElementById("forn").innerHTML = inputtext + " NOT FOUND";
+    ml5.objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "STATUS: DETECTING OBJECTS";
+}
+function modelLoaded() {
+    console.log("cocossd is loaded!");
+    status = true;
+}
+function draw() {
+    image(video,0, 0, 380, 380);
 }
